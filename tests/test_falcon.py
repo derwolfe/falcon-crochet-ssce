becc166082c7d18c8b2416e1bd683f525e289a56
Chris Wolfe <chriswwolfe@gmail.com>
2016-10-27 12:33:49 -0500

    working

--- tests/test_falcon.py
+++ tests/test_falcon.py
@@ -11,13 +11,12 @@ class APITest(testing.TestCase):
 
     def setUp(self):
         super(APITest, self).setUp()
+        self.app = server.api()
         crochet.setup()
 
     def test_endpoint_works(self):
         result = self.simulate_get('/threaded')
-        print(result.json)
 
-        serialized = json.loads(result)
-        keys = serialized.keys()
-        expected = ['statuses', 'alarm-state', 'last-update']
-        self.assertEqual(expected, keys)
+        expected = sorted(['statuses', 'alarm-state', 'last-update'])
+        received = sorted(result.json.keys())
+        self.assertEqual(expected, received)
